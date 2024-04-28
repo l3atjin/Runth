63b11011cd430af10a7d1cee80355d7ee8e2b1d0
Batjin Lamjav <l3atjin@gmail.com>
2024-04-27 17:40:58 -0700

    added api key

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+
+// AIzaSyDggMw_xFPUVoxoyW5_-HuOxtn4ReC0mWE
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
